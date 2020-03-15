566a9a5c1cb5138f8b55f3be63cc5db2a42bcb82
VitaliiTaranik <47079587+VitaliiTaranik@users.noreply.github.com>
2020-03-15 10:34:49 +0200

    Add files via upload

---
+++ work26/js/script.js
@@ -0,0 +1,24 @@
+$(function(){
+	//по клику
+
+	$('.list__inner .tab, .reviews__tab').on('click', function(event) {
+	var id = $(this).attr('data-id');
+		$('.list__inner, .reviews__inner').find('.tab-item, .reviews__content-item').removeClass('active-tab').hide();
+		$('.list__inner .tabs, .reviews__tabs').find('.tab, .reviews__tab').removeClass('active');
+		$(this).addClass('active');
+		$('#'+id).addClass('active-tab').fadeIn();
+		return false;
+	});
+
+	/*
+	//по ховеру
+	$('.list__inner .tab').hover(function(event) {
+	var id = $(this).attr('data-id');
+		$('.list__inner').find('.tab-item').removeClass('active-tab').hide();
+		$('.list__inner .tabs').find('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('#'+id).addClass('active-tab').fadeIn();
+		return false;
+	});*/
+
+});
