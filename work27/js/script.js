2c64b16d51192430c0679caa5cd48dac1e46f278
VitaliiTaranik <47079587+VitaliiTaranik@users.noreply.github.com>
2020-05-28 23:13:15 +0300

    Add files via upload

---
+++ work27/js/script.js
@@ -0,0 +1,8 @@
+//@prepros-append js-start.js
+//@prepros-append script.js
+//@prepros-append js-end.js
+// alert("Привет"),alert("mir");
+
+$('.nav__link-user').click(function(){
+    $('.header__form').slideToggle();
+});
