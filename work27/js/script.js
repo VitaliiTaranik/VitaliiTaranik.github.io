73d0fa1e0cc81572ed79fe155135354497b65bd8
VitaliiTaranik <47079587+VitaliiTaranik@users.noreply.github.com>
2020-05-31 10:20:39 +0300

    Add files via upload

--- work27/js/script.js
+++ work27/js/script.js
@@ -3,6 +3,17 @@
 //@prepros-append js-end.js
 // alert("Привет"),alert("mir");
 
-$('.nav__link-user').click(function(){
+$('.nav__link-user , .nav__burger-user').click(function(){
     $('.header__form').slideToggle();
 });
+
+// $('.header__burger__btn').click(function(){
+//     $('.header__burger_menu').slideToggle();
+// });
+
+$(document).ready(function(){
+	$('.header__burger__btn').click(function(){
+		$('.header__burger__btn').toggleClass('active')
+		$('.header__burger_menu').slideToggle()
+	})
+})
