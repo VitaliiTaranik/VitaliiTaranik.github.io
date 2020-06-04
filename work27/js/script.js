ac2d2bf7a93124d17dd85c8a4b55b789eb892162
VitaliiTaranik <47079587+VitaliiTaranik@users.noreply.github.com>
2020-06-04 10:40:18 +0300

    Add files via upload

--- work27/js/script.js
+++ work27/js/script.js
@@ -9,6 +9,9 @@ $('.nav__link-user , .nav__burger-user').click(function(){
 $('.header__close_btn').click(function(){
     $('.header__form').slideToggle();
 });
+$('.news__btn').click(function(){
+    $('.news__items_main-duble').slideToggle();
+});
 
 // $('.header__burger__btn').click(function(){
 //     $('.header__burger_menu').slideToggle();
@@ -20,3 +23,36 @@ $(document).ready(function(){
 		$('.header__burger_menu').slideToggle()
 	})
 })
+
+$(document).ready(function(){
+	$('.slider').slick({
+		speed:500,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		arrows: false,
+		dots:true,
+		adaptiveHeight: true,
+	});
+
+});
+
+$(document).ready(function(){
+	$('.feedback__slider_items').slick({
+		speed:500,
+		adaptiveHeight: true,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		arrows: true,
+		dots:true,
+		infinite: true,
+		responsive: [
+			{
+				breakpoint: 960,
+				settings: {
+					arrows:false,
+				}
+			}
+      ]
+	});
+
+});
