eed0e5c38a71aaf8a5f142f3a42bef78d86a9d2f
VitaliiTaranik <47079587+VitaliiTaranik@users.noreply.github.com>
2021-01-02 23:22:42 +0100

    Add files via upload

---
+++ work28/js/script.js
@@ -0,0 +1,45 @@
+let btns = document.querySelectorAll("*[data-modal-btn]");
+
+for (let i = 0; i < btns.length; i++) {
+   btns[i].addEventListener('click', function () {
+      let name = btns[i].getAttribute('data-modal-btn');
+      let modal = document.querySelector("[data-modal-window='" + name + "']");
+      modal.style.display = "block";
+      let close = modal.querySelector(".close_modal_window");
+      close.addEventListener('click', function () {
+         modal.style.display = "none";
+      })
+   })
+}
+
+window.onclick = function (e) {
+   if (e.target.hasAttribute('data-modal-window')) {
+      let modals = document.querySelectorAll("*[data-modal-window]");
+      for (let i = 0; i < modals.length; i++) {
+         modals[i].style.display = "none";
+      }
+   }
+}
+
+var accordion = function () {
+   var data = $(".accordion").attr("data-accordion");
+
+   $(".accordion__header").on("click", function () {
+      if (data === "close") {
+         $(".accordion__body").slideUp();
+         if ($(this).hasClass("active")) {
+            $(this).toggleClass("active");
+         }
+         else {
+            $(".accordion__header").removeClass("active");
+            $(this).toggleClass("active");
+         }
+      }
+      else {
+         $(this).toggleClass("active");
+      }
+      $(this).next(".accordion__body").not(":animated").slideToggle();
+   });
+}
+
+accordion()
